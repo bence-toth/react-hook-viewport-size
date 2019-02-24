2aa1a98fec900b904748446c2963a682933dfe5c
Bence A. Toth <5611525+bence-toth@users.noreply.github.com>
2019-02-24 12:58:00 +0100

    Added hook code

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import {useState, useEffect} from 'react'
+
+const useViewportSize = () => {
+  const [size, setSize] = useState({
+    width: window.innerWidth,
+    height: window.innerHeight
+  })
+
+  const handleViewportSizeChange = () => {
+    setSize({
+      width: window.innerWidth,
+      height: window.innerHeight
+    })
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleViewportSizeChange)
+    return () => {
+      window.removeEventListener('resize', handleViewportSizeChange)
+    }
+  }, [])
+
+  return [size.width, size.height]
+}
+
+export default useViewportSize
